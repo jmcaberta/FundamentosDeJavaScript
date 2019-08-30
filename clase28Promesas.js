91cd11271095f52152341d69d778d4f0d84d93e0
Juan Miguel Caberta <caber_t@hotmail.com>
2019-08-30 11:39:51 +0200

    Agregando la clase 28 Promesas

---
+++ clase28Promesas.js
@@ -0,0 +1,43 @@
+const API_URL = 'https://swapi.co/api/'
+
+const PEOPLE_URL = 'people/:id'
+
+const opts = {crossDomain: true}
+
+
+
+function obtenerPersonaje(id, callback) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, callback)
+    .fail (() => {
+        console.log(`Sucedio un error no se pudo obtener el personaje ${id}`)
+    }) 
+        
+} 
+
+
+
+obtenerPersonaje(1, function (persona) {    
+        console.log(`Hola, yo soy ${persona.name} `)    
+    obtenerPersonaje(2, function (persona) {
+        console.log(`Hola, yo soy ${persona.name} `)
+        obtenerPersonaje(3, function (persona) {
+            console.log(`Hola, yo soy ${persona.name} `)
+            obtenerPersonaje(4, function (persona) {
+                console.log(`Hola, yo soy ${persona.name} `)
+                obtenerPersonaje(5, function (persona) {
+                    console.log(`Hola, yo soy ${persona.name} `)
+                    obtenerPersonaje(6, function (persona) {
+                        console.log(`Hola, yo soy ${persona.name} `)
+                        obtenerPersonaje(7, function (persona) {
+                            console.log(`Hola, yo soy ${persona.name} `)
+                            obtenerPersonaje(8, function (persona) {
+                                console.log(`Hola Yo soy ${persona.name}`)
+                            })
+                        } )
+                    } )
+                } )
+            } )
+        } )
+    })
+})
