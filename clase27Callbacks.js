c4d4b0334b544ce2145799638821ce012ad37753
Juan Miguel Caberta <caber_t@hotmail.com>
2019-08-30 11:15:02 +0200

    Modificando Callbacks

--- clase27Callbacks.js
+++ clase27Callbacks.js
@@ -8,19 +8,24 @@ const opts = {crossDomain: true}
 
 function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, callback) {
-        console.log(`Hola, yo soy ${persona.name} `)  
-    }
+    $.get(url, opts, callback) 
 }
 
 
-obtenerPersonaje(1, function () {
-    obtenerPersonaje(2, function () {
-        obtenerPersonaje(3, function () {
-            obtenerPersonaje(4, function () {
-                obtenerPersonaje(5, function () {
-                    obtenerPersonaje(6, function () {
-                        obtenerPersonaje(7, function () {
+obtenerPersonaje(1, function (persona) {    
+        console.log(`Hola, yo soy ${persona.name} `)    
+    obtenerPersonaje(2, function (persona) {
+        console.log(`Hola, yo soy ${persona.name} `)
+        obtenerPersonaje(3, function (persona) {
+            console.log(`Hola, yo soy ${persona.name} `)
+            obtenerPersonaje(4, function (persona) {
+                console.log(`Hola, yo soy ${persona.name} `)
+                obtenerPersonaje(5, function (persona) {
+                    console.log(`Hola, yo soy ${persona.name} `)
+                    obtenerPersonaje(6, function (persona) {
+                        console.log(`Hola, yo soy ${persona.name} `)
+                        obtenerPersonaje(7, function (persona) {
+                            console.log(`Hola, yo soy ${persona.name} `)
                             obtenerPersonaje(8)
                         } )
                     } )
