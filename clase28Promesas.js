34a11353d5c0eb0c08207c92b546eb22bbb75b9a
Juan Miguel Caberta <caber_t@hotmail.com>
2019-09-02 13:36:18 +0200

    Changes in the class number 28

--- clase28Promesas.js
+++ clase28Promesas.js
@@ -1,30 +1,30 @@
-const API_URL = 'https://swapi.co/api/';
+const API_URL = 'https://swapi.co/api/'
 
-const PEOPLE_URL = 'people/:id';
+const PEOPLE_URL = 'people/:id'
 
-const opts = {crossDomain:true};
+const opts = {crossDomain:true}
 
 
 
 function obtenerPersonaje(id) {
     return new Promise((resolve, reject) => {
-        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
-        
-        $.get(url, opts, function (data) {
-            resolve (data)
-        })
-        .fail(() => reject(id))
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+            .get(url, opts, function (data) {
+              resolve(data)
+            })
+            .fail(() => reject(id))
     })  
         
 } 
 
 function onError(id) {
-    console.log(`Sucedio un error al obtener el personaje ${id}`)
+    console.log(`Sucedio un error al obtener el personaje: ${id}`)
 }
 
 obtenerPersonaje(1)
-    .then(function (persona) {
-        console.log(`El personaje 1 es: ${persona.name}`)
+    .then(function (personaje) {
+        console.log(`El personaje 1 es: ${personaje.name}`)
     })
 
     .catch(onError)
