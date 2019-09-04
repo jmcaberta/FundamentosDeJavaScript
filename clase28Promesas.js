8b43cf14c99b52975f90e9647c02bb516b6f3f48
Juan Miguel Caberta <caber_t@hotmail.com>
2019-09-04 13:57:37 +0200

    Add Class number 29

--- clase28Promesas.js
+++ clase28Promesas.js
@@ -1,15 +1,14 @@
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
-const opts = {crossDomain:true}
+const opts = { crossDomain: true }
 
 function obtenerPersonaje (id) {
     return new Promise((resolve, reject) => {
         const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-        $
-            .get(url, opts, function (data) {
-              resolve(data)
+        $.get(url, opts, function (data) {
+            resolve(data)
             })
-            .fail(() => reject(id))
+         .fail(() => reject(id))
     })  
         
 } 
